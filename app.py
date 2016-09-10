fb1d26dd5bcd940b3f29e12a6b8d99143068725c
volodymyrhusak <vovatrap@gmail.com>
2016-09-10 16:54:45 +0300

    add session

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
 from get_page import make_url, select_film, select_studio_film
 from select_insert_db import select_user
+from functools import wraps
 app = Flask(__name__)
+
+app.secret_key='secret'
+
+
+def login_required(f):
+	@wraps(f)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return f(*args, **kwargs)
+		else:
+			return redirect('/login')
+	return wrap
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template('home.html')
 
@@ -17,12 +31,14 @@ def login():
 		userName = request.form['userName']
 		password = request.form['password']
 		if select_user(userName,password):
+			session['logged_in'] = True
 			return redirect('/')
 		else:
 			return redirect('/login')
 
 
 @app.route('/post_film', methods=["POST"])
+@login_required
 def search_in_page():
     filmName = request.form['filmName']
     if not filmName:
@@ -32,6 +48,7 @@ def search_in_page():
 
 
 @app.route('/film/<idkinopoiskfilm>')
+@login_required
 def film_list(idkinopoiskfilm):
 
     filmData = select_film(idkinopoiskfilm)
@@ -40,15 +57,18 @@ def film_list(idkinopoiskfilm):
 
 
 @app.route('/studio_film/<studio>')
+@login_required
 def studio_film(studio):
 
     filmList = select_studio_film(studio)
     return render_template('studio_film.html', filmList=filmList)
 
 
-@app.route('/shutdown', methods=['POST'])
-def shutdown():
-    shutdown_server()
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    return redirect('/login')
 
 if __name__ == '__main__':
     app.run(debug=True)
